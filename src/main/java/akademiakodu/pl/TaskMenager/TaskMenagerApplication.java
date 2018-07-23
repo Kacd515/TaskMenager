cc184d92e03a2ccfd432c6e79680aee522148bf7
Kacper Slodzinski <ksodziski@gmail.com>
2018-07-23 10:55:30 +0200

    first commit

---
+++ src/main/java/akademiakodu/pl/TaskMenager/TaskMenagerApplication.java
@@ -0,0 +1,12 @@
+package akademiakodu.pl.TaskMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMenagerApplication.class, args);
+	}
+}
